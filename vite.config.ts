13918f2fc143d452ce18e8701f71f5e470da9221
Mejjad El Mehdi <mejjad@moenn-technologies.com>
2023-08-16 17:06:55 +0200

    [FIX]: add vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), svgr()],
+    base: "/",
+  };
 
-  plugins: [react(), svgr()],
+  if (command !== "serve") {
+    config.base = "/react-vite-gh-pages/";
+  }
+
+  return config;
 });
